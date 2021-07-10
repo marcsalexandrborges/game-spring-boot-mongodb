e99df4a78c6c9efd3edf87349f5ca2879592ba8a
Marcos Borges <marcsalexandr@gmail.com>
2021-07-10 10:01:11 -0300

    Projeto criado

---
+++ src/main/java/com/marcosborgesgamemongo/GamemongoApplication.java
@@ -0,0 +1,13 @@
+package com.marcosborgesgamemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamemongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamemongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcosborgesgamemongo/GamemongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosborgesgamemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamemongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
